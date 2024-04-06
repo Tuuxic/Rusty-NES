bf856892add32f990afda5c2d0d92f09540b885d
Nikolai Prjanikov <prjanikovnikolai@gmail.com>
2024-04-06 13:15:40 +0200

    Updated IO device to address ppu

--- rusty_nes/src/iodevice.rs
+++ rusty_nes/src/iodevice.rs
@@ -1,11 +1,9 @@
-use crate::bus_mod::bus::CpuRAM;
-
-const RAM_ADDR_MAX: u16 = 0x1FFF;
-const RAM_ADDR_MIN: u16 = 0x0000;
+use crate::{addr_utils::AddrUtils, bus_mod::bus::CpuRAM};
 
 pub struct IODevice<'a> {
     ram: &'a mut CpuRAM,
     // PPU
+    // ppu: &'a mut dyn Ppu
     // Cartrige
 }
 
@@ -15,18 +13,20 @@ impl IODevice<'_> {
     }
 
     pub fn cpu_write(&mut self, addr: u16, data: u8) {
-        if addr < RAM_ADDR_MIN || addr > RAM_ADDR_MAX {
-            return;
+        if addr >= AddrUtils::CPU_RAM_ADDR_MIN && addr <= AddrUtils::CPU_RAM_ADDR_MAX {
+            self.ram.write(addr & 0x07FF, data);
+        } else if addr >= AddrUtils::PPU_RAM_ADDR_MIN && addr <= AddrUtils::PPU_RAM_ADDR_MAX {
+            // self.ppu.cpu_write(addr & 0x0007, data);
         }
-
-        self.ram.write(addr & 0x07FF, data);
     }
 
     pub fn cpu_read(&mut self, addr: u16) -> u8 {
-        if addr < RAM_ADDR_MIN || addr > RAM_ADDR_MAX {
-            return 0x00;
+        if addr >= AddrUtils::CPU_RAM_ADDR_MIN && addr <= AddrUtils::CPU_RAM_ADDR_MAX {
+            return self.ram.read(addr & 0x07FF);
+        } else if addr >= AddrUtils::PPU_RAM_ADDR_MIN && addr <= AddrUtils::PPU_RAM_ADDR_MAX {
+            // return self.ppu.cpu_read(addr & 0x0007, true);
         }
 
-        self.ram.read(addr & 0x07FF)
+        0x00
     }
 }
