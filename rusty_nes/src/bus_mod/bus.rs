9845ed51a2c5af5cb6e39cb9a90651b030442279
Nikolai Prjanikov <prjanikovnikolai@gmail.com>
2024-04-06 14:13:47 +0200

    Replaced addr values in CpuRAM with constants

--- rusty_nes/src/bus_mod/bus.rs
+++ rusty_nes/src/bus_mod/bus.rs
@@ -1,9 +1,7 @@
-const RAM_SIZE: u32 = 2048; // 64 * 1024;
-const RAM_ADDR_MAX: u16 = 0xFFFF;
-const RAM_ADDR_MIN: u16 = 0x0000;
+use crate::addr_utils::AddrUtils;
 
 pub struct CpuRAM {
-    pub ram: [u8; RAM_SIZE as usize],
+    pub ram: [u8; AddrUtils::RAM_SIZE as usize],
     // read_func: fn(u16) -> u8,
     // write_func: fn(u16, u8)
 }
@@ -11,7 +9,7 @@ pub struct CpuRAM {
 impl CpuRAM {
     pub fn new() -> CpuRAM {
         let cpu_ram = CpuRAM {
-            ram: [0x00; RAM_SIZE as usize],
+            ram: [0x00; AddrUtils::RAM_SIZE as usize],
             // read_func: Bus::read,
             // write_func: Bus::write
         };
@@ -21,22 +19,22 @@ impl CpuRAM {
     }
 
     pub fn write(&mut self, addr: u16, data: u8) {
-        if addr < RAM_ADDR_MIN || addr > RAM_ADDR_MAX {
+        if addr < AddrUtils::CPU_RAM_ADDR_MIN || addr > AddrUtils::CPU_RAM_ADDR_MAX {
             return;
         }
 
         self.ram[addr as usize] = data;
     }
 
-    pub fn read_mut(&self, addr: u16, _read_only: bool) -> u8 {
-        if addr < RAM_ADDR_MIN || addr > RAM_ADDR_MAX {
+    pub fn read(&self, addr: u16) -> u8 {
+        self.read_mut(addr, false)
+    }
+
+    fn read_mut(&self, addr: u16, _read_only: bool) -> u8 {
+        if addr < AddrUtils::CPU_RAM_ADDR_MIN || addr > AddrUtils::CPU_RAM_ADDR_MAX {
             return 0x00;
         }
 
         self.ram[addr as usize]
-    }
-
-    pub fn read(&self, addr: u16) -> u8 {
-        self.read_mut(addr, false)
     }
 }
